aa19461a060ab0877ab1bae2d13d70623dccbebb
yuhyao <827623970@qq.com>
2024-01-07 00:58:04 +0800

    update 2D segment tree

--- datastruct/segtree-2d.cpp
+++ datastruct/segtree-2d.cpp
@@ -1,17 +1,17 @@
 /**
  * Author: Yuhao Yao
- * Date: 2022-10-23
+ * Date: 24-01-07
  * Description: 2D Segment Tree of range $[oL, oR] \times [iL, iR]$. Point apply and thus no lazy propogation.
  *  Always define a global $apply$ function to tell segment tree how you apply modification. 
  *  Combine is set as + operation. If you use your own struct, then please define constructor and + operation.
  *  In constructor, $q$ is the number of $pointApply$ you will use. $oL$, $oR$, Note that range parameters can be negative.
  * Usage: Point Add and Range (Rectangle) Sum.
- *  void apply(int \&a, int b) { a += b; } // global
+ *  void info_apply(int \&a, int b) { a += b; } // global
  *  ...
  *  SegTree2D<int> pseg(-5, 5, -5, 5, 1); // [-5, 5] * [-5, 5] and 1 update.
  *  int rt = 0; // empty node.
- *  rt = pseg.pointApply(rt, 2, -1, 1); // add 1 to position (2, -1).
- *  int sum = pseg.rangeAsk(rt, 3, 4, -2, -1); // ask the sum in rectangle [3, 4] * [-2, -1].
+ *  rt = pseg.point_apply(rt, 2, -1, 1); // add 1 to position (2, -1).
+ *  int sum = pseg.range_ask(rt, 3, 4, -2, -1); // ask the sum in rectangle [3, 4] * [-2, -1].
  * Time: O(\log (oR - oL + 1) \times \log(iR - iL + 1)) per operation.
  * Status: tested on https://qoj.ac/contest/819/problem/2548.
  */
@@ -33,14 +33,14 @@ struct SegTree2D {
 
 	// return new root id.
 	template<class... T>
-	int pointApply(int rt, int op, int ip, const T&... val) {
+	int point_apply(int rt, int op, int ip, const T&... val) {
 		auto idfs = [&](auto &dfs, int &i, int l, int r) {
 			if (!i) {
 				it.push_back({});
 				i = sz(it) - 1;
 			}
 			if (l == r) {
-				::apply(it[i].info, val...);
+				info_apply(it[i].info, val...);
 				return;
 			}
 			int mid = (l + r) >> 1;
@@ -64,7 +64,7 @@ struct SegTree2D {
 		return rt;
 	}
 
-	Info rangeAsk(int rt, int qol, int qor, int qil, int qir) {
+	Info range_ask(int rt, int qol, int qor, int qil, int qir) {
 		Info res{};
 		auto idfs = [&](auto &dfs, int i, int l, int r) {
 			if (!i || qir < l || r < qil) return;
