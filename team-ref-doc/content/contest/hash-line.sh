9af2b07cf60e965c8c8122ef8154f214e48fdd92
yuhyao <827623970@qq.com>
2024-01-10 09:32:02 +0800

    Update hash-line.sh

--- team-ref-doc/content/contest/hash-line.sh
+++ team-ref-doc/content/contest/hash-line.sh
@@ -1,7 +1,7 @@
 # Hashes each line of given file.
 # Usage: $ sh hash-line.sh hash-cpp.sh code.cpp
 IFS=''
-while read line; do
+cat $2 | while read line; do
   echo $line | sh $1 | cut -c-2 | tr -d '\n'
   echo " "$line
-done < $2
+done
